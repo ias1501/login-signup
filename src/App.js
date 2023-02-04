40eabf0f691127c79a74f3536a51dd0fedf87bfb
ias1501 <103571149+ias1501@users.noreply.github.com>
2023-02-04 22:37:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="cover">
+      <div className="mb-0">
+        <h1>SIGN IN </h1>
+      </div>
+      <div className="mb-1">
+        <h2>
+          Sign in now and register for upcoming events and future announcements.
+        </h2>
+      </div>
+
+      <div className="mb-3"></div>
+
+      <div className="mb-2">
+        <input type="text" placeholder="Email" />
+      </div>
+
+      <div className="mb-3"></div>
+
+      <div className="mb-4"></div>
+
+      <div className="mb-5">
+        <a href="https://mail.google.com/mail/u/0/#inbox"> Get Link </a>
+      </div>
+
+      <div className="mx-3"></div>
+      <div className="mt-2"></div>
+
+      <div className="mt-3"></div>
+
+      <div className="mx-4"></div>
+      <div className="ml-5"></div>
+      <div className=""></div>
+    </div>
+  );
+}
